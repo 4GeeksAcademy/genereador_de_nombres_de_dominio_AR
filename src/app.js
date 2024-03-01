4593edf47b89b0baa0eb243b4b85375066beb2ef
Aitana Rodriguez <144685094+AitanaRQ@users.noreply.github.com>
2024-03-01 18:43:07 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -4,8 +4,25 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronouns = ["the", "our"];
+  let adjectives = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+  let extensions = [".com", ".net", ".us", ".io", ".es"];
+
+  for (let i = 0; i < pronouns.length; i++) {
+    for (let j = 0; j < adjectives.length; j++) {
+      for (let k = 0; k < nouns.length; k++) {
+        for (let l = 0; l < extensions.length; l++) {
+          let list = document.createElement("li");
+          list.className = "text-light";
+          list.innerText = `${pronouns[i]}${adjectives[j]}${nouns[k]}${extensions[l]}`;
+          document.querySelector("#domains").appendChild(list);
+          console.log(
+            `${pronouns[i]}${adjectives[j]}${nouns[k]}${extensions[l]}`
+          );
+        }
+      }
+    }
+  }
 };
